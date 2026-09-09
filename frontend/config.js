59a1872572fb39db6691e54f6a75b044544f321c
Wittaya-Tasu <vetza.yayo@gmail.com>
2026-09-09 14:48:02 +0700

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 // Public connection settings only. Never place a secret, password, or user list here.
 window.SVS_CONFIG = {
-  apiUrl: '', // https://script.google.com/macros/s/DEPLOYMENT_ID/exec
+  apiUrl: '', https://script.google.com/macros/s/AKfycbwKLkyVPhGLIYxo09qWRTBrxS1AmVWRPWkiW7jQ97sRhEVQCu32W_4bC52tuJOZ3_qY/exec
   appName: 'SVS Health Dashboard'
 };
